8f8bbb91abbe00b3561723d13d1b2ba7807fee7a
Jordan Landry <jordanlandry1999@gmail.com>
2023-03-27 17:42:35 -0400

    Updated a project link

--- src/data/otherProjectData.ts
+++ src/data/otherProjectData.ts
@@ -20,7 +20,7 @@ const otherProjects = [
   {
     name: "Rubiks Cube Solver",
     description: "Made with ThreeJs, a 3D animated Rubik's Cube solver.",
-    githubLink: "https://github.com/jordanlandry/rubiks-cube-solver",
+    githubLink: "https://github.com/jordanlandry/rubiks-cube",
     language: "TypeScript",
     icon: rubiksCube,
     languageIcon: "https://cdn.worldvectorlogo.com/logos/typescript.svg",
